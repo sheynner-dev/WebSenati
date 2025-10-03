12f7d3834d91de6382a6e11c4baa6d91dcf85d36
sasasasasaaaaaaasss <zavalasheynner@gmail.com>
2025-10-02 22:03:23 -0500

    Build listo para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  plugins: [react()],
   base: '/WebSenati/',
-   build: {
+  build: {
     outDir: 'docs'
   }
-})
+});
